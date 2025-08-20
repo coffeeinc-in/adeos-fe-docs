e4f88c8102e88e7b43a5e67a666885973adc9c2d
vijay-fs <vijaykarthik9868@gmail.com>
2025-08-20 11:44:54 +0530

    prefill-file data

--- content/demo-v1/_meta.ts
+++ content/demo-v1/_meta.ts
@@ -2,4 +2,5 @@ export default {
   index: 'Approach',
   "step-one-sample-files": 'Step 1: Approach for sample files',
   "step-two-handle-file-data": 'Step 2: Handle File data',
+  "step-three-prefill-file-data": 'Step 3: Prefill File data',
 }
