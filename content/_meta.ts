6561a2c58e06980f848c7f6c1a95f64fb168212e
vijay-fs <vijaykarthik9868@gmail.com>
2025-09-09 16:14:58 +0530

    docs: add Sprint 02 ADEOS FE to content metadata

--- content/_meta.ts
+++ content/_meta.ts
@@ -1,4 +1,5 @@
 export default {
   index: 'Home',
   "demo-v1": 'Demo v1',
+  "sprint-02": 'Sprint 02 ( ADEOS FE )',
 }
---
+++ content/sprint-02/_meta.ts
@@ -0,0 +1,5 @@
+export default {
+  index: 'Sprint 02 (SEP 09 25)',
+  "websocket": 'Websocket',
+  "adeos-ui-optimizations": 'UI Component Optimizations',
+}
