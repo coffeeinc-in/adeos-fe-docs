9c533bed104c2788c2384152f0fa7fc8ba13b2db
vijay <85994748+vijay-fs@users.noreply.github.com>
2025-10-03 12:50:30 +0530

    v2 iteration flow for file Upload

--- content/_meta.ts
+++ content/_meta.ts
@@ -1,6 +1,7 @@
 export default {
   index: 'Home',
   "existing-flow": 'Existing Flow',
+  "v2-iteration": 'V2 Iteration',
   "tax-sprint": 'Tax Sprint',
   "demo-version": 'Demo Version',
 }
---
+++ content/v2-iteration/_meta.ts
@@ -0,0 +1,5 @@
+export default {
+  index: 'V2 Iteration Overview',
+  'file-upload-frontend': 'File Upload - Frontend',
+  'file-upload-backend': 'File Upload - Backend',
+}
