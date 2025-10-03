e7bacb5dc1e98e695b9714da4825cbc3050348f7
vijay <85994748+vijay-fs@users.noreply.github.com>
2025-10-03 13:21:01 +0530

    Cleared unwanted docs

--- content/_meta.ts
+++ content/_meta.ts
@@ -2,6 +2,6 @@ export default {
   index: 'Home',
   "existing-flow": 'Existing Flow',
   "v2-iteration": 'V2 Iteration',
-  "tax-sprint": 'Tax Sprint',
   "demo-version": 'Demo Version',
+  "tools": 'Tools',
 }
--- content/tax-sprint/_meta.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  index: 'Sprint 02 (SEP 09 25)',
-  "websocket": 'Websocket',
-  "websocket-v2": 'Websocket v2',
-  "adeos-ui-optimizations": 'UI Component Optimizations',
-}
